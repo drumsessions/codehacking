308b276755fb67abfbd91c1025a060cfea7837d3
Rne Bustamante <drumsessions@gmail.com>
2018-01-17 20:41:59 -0700

    Able to create posts

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use App\Http\Requests\PostsCreateRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Photo;
 
 class AdminPostsController extends Controller
 {
@@ -14,6 +18,8 @@ class AdminPostsController extends Controller
     public function index()
     {
         //
+        $posts = Post::all();
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -24,6 +30,7 @@ class AdminPostsController extends Controller
     public function create()
     {
         //
+        return view('admin.posts.create');
     }
 
     /**
@@ -32,9 +39,24 @@ class AdminPostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostsCreateRequest $request)
     {
         //
+        $input = $request->all();
+        $user = Auth::user();
+
+        if ($file = $request->file('photo_id')) {
+            $name = Time().'_'.$file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['file'=>$name]);
+            $input['photo_id'] = $photo->id;
+        }
+        //$input['user_id'] = $user->id;
+        $input['category_id'] = 1;
+
+        $user->posts()->create($input);
+
+        return redirect('/admin/posts');
     }
 
     /**
